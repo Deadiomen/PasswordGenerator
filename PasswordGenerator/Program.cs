5320767dcf0bc3531e6f0d183730fe66d68d4a55
Moro <91848845+Deadiomen@users.noreply.github.com>
2024-03-16 17:58:04 +0100

    Update Program.cs

--- PasswordGenerator/Program.cs
+++ PasswordGenerator/Program.cs
@@ -7,17 +7,17 @@
             Random random = new Random();
             int lengtePassword = 0;
 
-            Console.WriteLine("*****************************\nWelkom bij Password Generator\n*****************************");
+            Console.WriteLine("*****************************\nWelcome to Password Generator\n*****************************");
 
             while(true)
 
             {
-                Console.WriteLine("Hoelang moet je wachtwoord zijn? (min: 8 tekens / max: 20 tekens)");
+                Console.WriteLine("how long should your password be? (min: 8 characters / max: 20 characters)");
                 lengtePassword = int.Parse(Console.ReadLine());
 
                 if (lengtePassword >= 8 && lengtePassword <= 20)
                 {
-                    Console.Write("Wachtwoord: ");
+                    Console.Write("Password: ");
                     for (int i = 0; i < lengtePassword; i++)
                     {
                         char randomPassword = Convert.ToChar(random.Next(33, 123));
@@ -30,7 +30,7 @@
                 }
                 else
                 {
-                    Console.WriteLine("Ongeldige lengte, probeer opnieuw.");
+                    Console.WriteLine("Invalid length, please try again.");
                    
                 }
                 
