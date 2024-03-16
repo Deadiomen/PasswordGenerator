c2b228fefe07e8c0b5d48eb06fe1379aac49c3c7
Moro <mounirdelil12@gmail.com>
2024-03-16 17:54:36 +0100

    Add project files.

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int lengtePassword = 0;
+
+            Console.WriteLine("*****************************\nWelkom bij Password Generator\n*****************************");
+
+            while(true)
+
+            {
+                Console.WriteLine("Hoelang moet je wachtwoord zijn? (min: 8 tekens / max: 20 tekens)");
+                lengtePassword = int.Parse(Console.ReadLine());
+
+                if (lengtePassword >= 8 && lengtePassword <= 20)
+                {
+                    Console.Write("Wachtwoord: ");
+                    for (int i = 0; i < lengtePassword; i++)
+                    {
+                        char randomPassword = Convert.ToChar(random.Next(33, 123));
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(randomPassword);
+                        
+                    }
+                    Console.WriteLine( );
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ongeldige lengte, probeer opnieuw.");
+                   
+                }
+                
+            }
+            Console.ForegroundColor= ConsoleColor.White;    
+            
+
+        }
+    }
+}
